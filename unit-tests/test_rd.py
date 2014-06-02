5ed9557782ebd093834f22f9178a5e331f1d8884
Fred Phillips <fophillips@fophillips.org>
2014-06-02 14:39:33 +0100

    Rewrite unit tests to expect functional form, use better values of D and dt, and increase size of system

--- unit-tests/test_rd.py
+++ unit-tests/test_rd.py
@@ -5,26 +5,28 @@ from rd import RD
 
 class TestRD:
     def setup(self):
-        self.init_conc = np.array([0, 0, 0])
-        self.reaction = np.array([0, 1, 0])
-        self.diff = np.array([1, 1, 1])
-        self.rd = RD(self.reaction, self.diff)
+        self.init_conc = np.array([0, 0, 0, 0, 0])
+        self.reaction = np.array([0, 0, 1, 0, 0])
+        self.diff = np.array([0.1, 0.1, 0.1, 0.1, 0.1])
+        self.dt = 0.5
+        self.dx = 1
 
     def test_one_timestep(self):
-        expected = np.array([0, 1, 0])
-        actual = self.rd.next_concentrations(self.init_conc)
-        assert(np.array_equal(expected, actual))
+        expected = np.array([0, 0, 0.5, 0, 0])
+        actual = RD.integrate(1, self.init_conc, self.reaction, self.diff, self.dt, self.dx)
+        self.check_arrays(expected, actual)
         
     def test_two_timesteps(self):
-        expected = np.array([1, 0, 0])
-        actual = self.rd.n_timesteps(self.init_conc, 2)
-        print("Expected:",expected)
-        print("Actual:",actual)
-        assert(np.array_equal(expected, actual))
+        expected = np.array([0, 0.025, 0.95, 0.025, 0])
+        actual = RD.integrate(2, self.init_conc, self.reaction, self.diff, self.dt, self.dx)
+        self.check_arrays(expected, actual)
 
     def test_three_timesteps(self):
-        expected = np.array([0, 2, 0])
-        actual = self.rd.n_timesteps(self.init_conc, 3)
+        expected = np.array([0.00125, 0.07, 1.3575, 0.07, 0])
+        actual = RD.integrate(3, self.init_conc, self.reaction, self.diff, self.dt, self.dx)
+        self.check_arrays(expected, actual)
+
+    def check_arrays(self, expected, actual):
         print("Expected:",expected)
         print("Actual:",actual)
-        assert(np.array_equal(expected, actual))
+        assert(np.allclose(expected, actual))
