6c8226b1747d6af205bd9c32ffaa0db48be0e2b0
Fred Phillips <fophillips@fophillips.org>
2014-06-02 14:55:29 +0100

    Rewrite RD to make new tests pass

--- rd.py
+++ rd.py
@@ -1,34 +1,30 @@
 import numpy as np
 
 class RD:
-    def __init__(self, reaction, diffusivities, dt, dx):
-        self.reaction = reaction
-        self.diff = diffusivities
-        self.dt = dt
-        self.dx = dx
-        
-    def next_concentrations(self, conc):
+    @classmethod
+    def next_concentration(cls, conc, reac, diff, dt, dx):
         next_conc = np.empty(conc.shape)
         for i in range(conc.size):
             if(i == 0):
                 next_conc[i] = conc[i] \
-                               + 0.5 * self.dt / self.dx**2 \
-                               * (self.diff[i+1] + self.diff[i]) * (conc[i+1] - conc[i]) \
-                               + self.reaction[i] * self.dt
+                               + 0.5 * dt / dx**2 \
+                               * (diff[i+1] + diff[i]) * (conc[i+1] - conc[i]) \
+                               + reac[i] * dt
             elif(i == conc.size-1):
                 next_conc[i] = 0
             else:
                 next_conc[i] = conc[i] \
-                               + 0.5 * self.dt / self.dx**2 \
-                               * ((self.diff[i+1] + self.diff[i]) * (conc[i+1] - conc[i]) \
-                               + (self.diff[i-1] + self.diff[i]) * (conc[i-1] - conc[i])) \
-                               + self.reaction[i] * self.dt
+                               + 0.5 * dt / dx**2 \
+                               * ((diff[i+1] + diff[i]) * (conc[i+1] - conc[i]) \
+                               + (diff[i-1] + diff[i]) * (conc[i-1] - conc[i])) \
+                               + reac[i] * dt
             if next_conc[i] < 0:
                 next_conc[i] = 0
         return next_conc
 
-    def n_timesteps(self, conc, n):
+    @classmethod
+    def integrate(cls, n, conc, reac, diff, dt, dx):
         next_conc = conc
         for _ in range(n):
-            next_conc = self.next_concentrations(next_conc)
+            next_conc = cls.next_concentration(next_conc, reac, diff, dt, dx)
         return next_conc
